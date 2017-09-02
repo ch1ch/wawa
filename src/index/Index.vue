47247add13ba62cc31ef38d0eb04296b0ee8eafe
fu <fjchao@gmail.com>
2017-09-02 23:58:06 +0800

    init

---
+++ src/index/Index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="header">
+  	<div class="headerimg">
+  		<img src="./../assets/images/header1.jpg">		
+  	</div>
+    
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style lang="less">
+	@import './../assets/less/main.less';
+</style>]
