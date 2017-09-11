0a6bc4392d8d99f37b31b8568e66e8341b423857
fuask <fuask@qq.com>
2017-09-11 22:40:00 +0800

    live

--- client/src/components/Livepage.vue
+++ client/src/components/Livepage.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="hello">
-  <!-- <remote-js src="http://www.legendream.cn/myjs/tcplayer.js"></remote-js> -->
-    <h1>{{ msg }}</h1>
-    <div id="id_test_video" style="width:100%; height:auto;"></div>
+  <div class="live">
+    
+    <a href="./..">
+      <div class="back">
+        <img src="./../assets/images/live/back.png"> 
+      </div>
+    </a>
+    <div class="livebox">
+    </div>
+    <third-js src="http://www.legendream.cn/myjs/tcplayer.js"></third-js>
+    <div class="gamestart">
+    </div>
+    <div class="gameinfo"></div>
+    <!-- <h1>{{ msg }}</h1> -->
+    <div id="id_test_video"></div>
   </div>
 </template>
 
@@ -20,9 +31,9 @@ export default {
     }
   },
   components: {
-   'remote-js': {
+   'third-js': {
     render(createElement) {
-      return createElement('script', { attrs: { type: 'text/javascript', src: this.src }});
+      return createElement('script', { attrs: { type: 'text/javascript', src: "./../assets/js/tcplayer.js" }});
       }
     }
   },
