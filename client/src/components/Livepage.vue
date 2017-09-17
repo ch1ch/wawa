dd96e1db35b74bd2f68eb88902d0fcb6b596ddd9
fu <fjchao@gmail.com>
2017-09-17 23:48:04 +0800

    娃娃机链接

--- client/src/components/Livepage.vue
+++ client/src/components/Livepage.vue
@@ -25,9 +25,10 @@
 
 <script>
 // const BaseUrl = "http://192.168.1.229:9001/";
-// const BaseUrl = "http://fjchao.vicp.io:23822/";
-// const BaseUrl = "http://fjchao.f3322.net:9001/";
-const BaseUrl = "http://83645590.800.si/";
+// const BaseUrl = "http://fjchao.vicp.io:9001/";
+const BaseUrl = "http://fjchao.in.3322.org:42404/";
+// const BaseUrl = "http://fjchao.7766.org:9001/";
+// const BaseUrl = "http://90946475.800.si/";
 //47.94.236.45:9000
 
 
@@ -48,7 +49,7 @@ export default {
   },
   mounted:function(){
     var player =  new TcPlayer('id_test_video', {
-        // "m3u8": "http://10799.liveplay.myqcloud.com/live/10799_784387bddc.m3u8",
+        "m3u8": "http://10799.liveplay.myqcloud.com/live/10799_784387bddc.m3u8",
         "flv": "http://10799.liveplay.myqcloud.com/live/10799_784387bddc.flv", //增加了一个flv的播放地址，用于PC平台的播放 请替换成实际可用的播放地址
         "autoplay" : true,      //iOS下safari浏览器，以及大部分移动端浏览器是不开放视频自动播放这个能力的
         "coverpic" : "http://www.legendream.cn/myjs/start.png",
